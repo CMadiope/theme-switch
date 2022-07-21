38befcb572c139f927f2d292d441635a2a0f4654
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 17:34:10 +0200

    ADD: theme props to Cards.styled

--- src/Components/styles/Cards.styled.js
+++ src/Components/styles/Cards.styled.js
@@ -2,26 +2,26 @@ import styled from "styled-components";
 
 export const CardsContainer = styled.section`
   margin: 50px;
-`
+`;
 export const Card = styled.div`
-  background-color: hsl(60, 40%, 100%);
-  border: 1px solid hsl(0, 0%, 87%);
+  background-color: ${({ theme }) => theme.colors.quoteBgc};
+  border: 1px solid ${({ theme }) => theme.colors.quoteBorder};
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 20px;
   border-radius: 3px;
   max-width: 450px;
-`
+`;
 
 export const CardTitle = styled.div`
-  color: hsl(0, 1%, 38%);
-  border-bottom: 1px solid hsl(0, 0%, 87%);
+  color: ${({theme}) => theme.colors.quoteTitle};
+  border-bottom: 1px solid ${({theme}) => theme.colors.quoteBorder};
   text-align: center;
   padding: 10px;
   font-weight: bold;
-`
+`;
 
 export const CardBody = styled.div`
-  color: hsl(0, 1%, 38%);
+  color: ${({theme}) => theme.colors.quoteBody};
   padding: 10px;
-`
+`;
