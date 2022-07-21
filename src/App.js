04beee8536ec964f021fda3e5a2a50b702d115b6
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 17:59:22 +0200

    ADD: useEffcet hook to get stored theme

--- src/App.js
+++ src/App.js
@@ -15,15 +15,23 @@ import {
   brown,
   pink,
 } from "./Components/styles/Theme.styled";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   //theme state
   const [selectedTheme, setSelectedTheme] = useState(light);
+  //react hook to get the theme selected by user that is saved in local storage
+  useEffect(() => {
+    const currentTheme = JSON.parse(localStorage.getItem('current-theme'))
+    if (currentTheme) {
+      setSelectedTheme(currentTheme)
+    }
+  }, [])
 
   //function to handle user them selection on click and save it to local storage
   const HandleThemeChange = (theme) => {
     setSelectedTheme(theme);
+    localStorage.setItem('current-theme', JSON.stringify(theme))
   };
   return (
     <ThemeProvider theme={selectedTheme}>
@@ -39,23 +47,23 @@ function App() {
           ></ThemeButton>
           <ThemeButton
             className={`dark ${selectedTheme === dark ? "active" : ""}`}
-            onClick={() => HandleThemeChange(light)}
+            onClick={() => HandleThemeChange(dark)}
           ></ThemeButton>
           <ThemeButton
             className={`blue ${selectedTheme === blue ? "active" : ""}`}
-            onClick={() => HandleThemeChange(light)}
+            onClick={() => HandleThemeChange(blue)}
           ></ThemeButton>
           <ThemeButton
             className={`green ${selectedTheme === green ? "active" : ""}`}
-            onClick={() => HandleThemeChange(light)}
+            onClick={() => HandleThemeChange(green)}
           ></ThemeButton>
           <ThemeButton
             className={`brown ${selectedTheme === brown ? "active" : ""}`}
-            onClick={() => HandleThemeChange(light)}
+            onClick={() => HandleThemeChange(brown)}
           ></ThemeButton>
           <ThemeButton
             className={`pink ${selectedTheme === pink ? "active" : ""}`}
-            onClick={() => HandleThemeChange(light)}
+            onClick={() => HandleThemeChange(pink)}
           ></ThemeButton>
         </ThemeContainer>
 
