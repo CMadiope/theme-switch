3f9efbe6e0fd1872f7e0ca56d6c5652b51542587
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 16:45:00 +0200

    ADD: themecotainer styled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {GlobalStyles} from './Components/styles/Global'
 import {Header} from './Components/styles/Header.styled'
 import {Footer} from './Components/styles/Footer.styled'
+import Quotes from './Components/Quotes';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyles />
       <Header>Game of Thrones Quotes</Header>
+
+        <Quotes/>
+
       <Footer>
         <p>
           Made by <a href='https://github.com/CMadiope'>Confidence</a>
