443698efb316265843375cc6c41e7506567f6f36
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 16:48:49 +0200

    import ThemeSwitching in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import {GlobalStyles} from './Components/styles/Global'
 import {Header} from './Components/styles/Header.styled'
 import {Footer} from './Components/styles/Footer.styled'
 import Quotes from './Components/Quotes';
+import { ThemeContainer, ThemeButton } from './Components/styles/ThemeSwitching.styled';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyles />
       <Header>Game of Thrones Quotes</Header>
+
+      <ThemeContainer>
+        <span>Themes: </span>
+        <ThemeButton className='light active'></ThemeButton>
+        <ThemeButton className='dark'></ThemeButton>
+        <ThemeButton className='blue'></ThemeButton>
+        <ThemeButton className='green'></ThemeButton>
+        <ThemeButton className='brown'></ThemeButton>
+        <ThemeButton className='pink'></ThemeButton>
+      </ThemeContainer>
 
         <Quotes/>
 
