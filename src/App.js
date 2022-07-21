19edae62bd87cc8578f3d8eca34a329240629fd5
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 17:09:03 +0200

    import themeprovider and themes in app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import {GlobalStyles} from './Components/styles/Global'
-import {Header} from './Components/styles/Header.styled'
-import {Footer} from './Components/styles/Footer.styled'
-import Quotes from './Components/Quotes';
-import { ThemeContainer, ThemeButton } from './Components/styles/ThemeSwitching.styled';
+import { GlobalStyles } from "./Components/styles/Global";
+import { Header } from "./Components/styles/Header.styled";
+import { Footer } from "./Components/styles/Footer.styled";
+import Quotes from "./Components/Quotes";
+import {
+  ThemeContainer,
+  ThemeButton,
+} from "./Components/styles/ThemeSwitching.styled";
+import { ThemeProvider } from "styled-components";
+import {
+  light,
+  dark,
+  blue,
+  green,
+  brown,
+  pink,
+} from "./Components/styles/Theme.styled";
 
 function App() {
   return (
-    <div className='App'>
-      <GlobalStyles />
-      <Header>Game of Thrones Quotes</Header>
+    <ThemeProvider theme= {light}>
+      <div className='App'>
+        <GlobalStyles />
+        <Header>Game of Thrones Quotes</Header>
 
-      <ThemeContainer>
-        <span>Themes: </span>
-        <ThemeButton className='light active'></ThemeButton>
-        <ThemeButton className='dark'></ThemeButton>
-        <ThemeButton className='blue'></ThemeButton>
-        <ThemeButton className='green'></ThemeButton>
-        <ThemeButton className='brown'></ThemeButton>
-        <ThemeButton className='pink'></ThemeButton>
-      </ThemeContainer>
+        <ThemeContainer>
+          <span>Themes: </span>
+          <ThemeButton className='light active'></ThemeButton>
+          <ThemeButton className='dark'></ThemeButton>
+          <ThemeButton className='blue'></ThemeButton>
+          <ThemeButton className='green'></ThemeButton>
+          <ThemeButton className='brown'></ThemeButton>
+          <ThemeButton className='pink'></ThemeButton>
+        </ThemeContainer>
 
-        <Quotes/>
+        <Quotes />
 
-      <Footer>
-        <p>
-          Made by <a href='https://github.com/CMadiope'>Confidence</a>
-        </p>
-      </Footer>
-    </div>
+        <Footer>
+          <p>
+            Made by <a href='https://github.com/CMadiope'>Confidence</a>
+          </p>
+        </Footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/Components/styles/Theme.styled.js
+++ src/Components/styles/Theme.styled.js
@@ -1,3 +1,4 @@
+
 export const light = {
   name: "light-theme",
   colors: {
